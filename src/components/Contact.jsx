1ccebb02d84a8a62928d1563c1cb854c382c2406
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-08-01 09:26:24 -0600

    initial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+
+const Contact = () => {
+  return (
+    <>
+      <h1>Contact</h1>
+      <p>lorem ipsum</p>
+    </>
+  )
+}
+
+export default Contact
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>lorem ipsum</p>
+    </>
+  )
+}
+
+export default Home
