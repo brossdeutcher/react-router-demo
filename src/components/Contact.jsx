218763045398b564c9915b397ccc8f0c24e40a77
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-08-01 09:46:56 -0600

    added Contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react"
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNum, setPhoneNum] = useState('');
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    alert(`Name: ${name}\nEmail: ${email}\nPhone: ${phoneNum}`);
+  }
+
   return (
     <>
       <h1>Contact</h1>
+      <form onSubmit={formSubmit}>
+        <label>
+          Name
+          <input value={name} onChange={(e) => {setName(e.target.value)}} />
+        </label>
+        <label>
+          Email
+          <input value={email} onChange={(e) => {setEmail(e.target.value)}} />
+        </label>
+        <label>
+          phoneNum
+          <input value={phoneNum} onChange={(e) => {setPhoneNum(e.target.value)}} />
+        </label>
+        <br />
+        <button type="submit">Submit!!</button>
+      </form>
       <p>lorem ipsum</p>
     </>
   )
